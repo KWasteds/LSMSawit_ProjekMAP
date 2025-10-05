c1c3988618d301919fc44e30332ba9e2aa48724b
kayoo18 <92360576+kayoo18@users.noreply.github.com>
2025-10-05 21:46:04 +0700

    ilangin garis ijo di login/register sama ganti warna dalemnya

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,7 @@ dependencies {
     implementation("androidx.cardview:cardview:1.0.0")
 
     implementation(platform(libs.firebase.bom))
+    implementation("com.google.android.material:material:1.12.0")
 
     // Add the dependencies for Firebase products you want to use
     // The versions are managed by the BoM
--- app/src/main/java/com/example/lsmsawit_projekmap/MainActivity.kt
+++ app/src/main/java/com/example/lsmsawit_projekmap/MainActivity.kt
@@ -15,9 +15,6 @@ import com.example.lsmsawit_projekmap.databinding.ActivityMainBinding
 import com.google.firebase.FirebaseApp
 import com.google.firebase.firestore.FirebaseFirestore
 import android.util.Log
-import com.example.lsmsawit_projekmap.R
-
-
 
 class MainActivity : AppCompatActivity() {
 
@@ -27,32 +24,37 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // Inisialisasi Firebase (opsional, tapi biar aman)
+        FirebaseApp.initializeApp(this)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Toolbar setup
         setSupportActionBar(binding.appBarMain.toolbar)
 
+        // FAB
         binding.appBarMain.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null)
                 .setAnchorView(R.id.fab).show()
         }
+
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+
         appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
             ), drawerLayout
         )
+
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.main, menu)
         return true
     }
@@ -61,5 +63,4 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
-
 }
